85b22016336f65c68abff2b6247590525dea8f5a
YuMin Kim <55650732+devYuMinKim@users.noreply.github.com>
2023-07-01 19:16:24 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    
+    cin >> A;
+    cin >> B;
+    
+    cout << A + B << "\n";
+    cout << A - B << "\n";
+    cout << A * B << "\n";
+    cout << A / B << "\n";
+    cout << A % B;
+    
+    return 0;
+}
