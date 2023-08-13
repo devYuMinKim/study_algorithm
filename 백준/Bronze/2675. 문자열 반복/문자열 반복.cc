86ea5311591033f8dfecbb008fa5106b2fdb2bd5
YuMin Kim <55650732+devYuMinKim@users.noreply.github.com>
2023-08-14 00:15:11 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t, R;
+	string s;
+
+	cin >> t;
+
+	while (t--) {
+		cin >> R >> s;
+
+		for (int i = 0; i < s.length(); i++) {
+			for (int k = 0; k < R; k++) {
+				cout << s[i];
+			}
+		}
+		cout << '\n';
+	}
+
+    return 0;
+}
